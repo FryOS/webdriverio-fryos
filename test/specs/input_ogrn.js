18116e44239eb1c5845c5bc1e74d8cde21b59df6
Alexey <alexeyosp@gmail.com>
2018-05-23 21:21:04 +0300

    input ogrn and click

--- test/specs/input_ogrn.js
+++ test/specs/input_ogrn.js
@@ -3,14 +3,25 @@ describe('input ogrn', function()  {
 		browser
 		.url('/')
 		.element('a[href="/lights"]')
-		.click()
+		.click()	
+		.waitForVisible('input[name="ogrn"]', 3000)
+		$('input[name="ogrn"]')
+		.setValue('1165275036607')
+		$('//*[@id="lightsBlock"]/div/div/div[3]/div/div[2]/div/div/button').click()
 		
-		.waitUntil(function(){
-			return $('input[name="ogrn"]') //input[name="ogrn"]
-		},10000, 'вышло время')
 		
-	  .setValue('//*[@id="ogrn-undefined--32745"]', 123)
-		.pause(3000);
-		console.log('elem ' + elem);
 	})
 })
+
+
+// describe('input ogrn', function()  {
+// 	it('input ogrn', function() {
+// 		browser
+// 		.url('/')
+// 		.element('a[href="/lights"]')
+// 		.click()
+// 		.pause(3000)
+// 		$('input[name="ogrn"]') //input[name="ogrn"]
+// 		.setValue('123')
+// 	})
+// })
